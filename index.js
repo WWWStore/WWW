261ea64fcdb2ba940251bd3d9a3099cdd961d5a0
ColoSRJones <colosrjones@gmail.com>
2019-10-14 14:41:32 -0500

    Added MongoDB and it works

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 'use strict';
 
 const server = require('./src/server');
+const mongoose = require('mongoose');
 require('dotenv').config();
+
+const mongooseOptions = {
+  useNewUrlParser:true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+};
+
+mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
 
 server.start(process.env.PORT);
