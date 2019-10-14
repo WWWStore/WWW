2363ea4d5028dc6c083d29364ddc3f6f7b040ccd
Jon Struve <ocdammo3@gmail.com>
2019-10-14 15:09:00 -0500

    Added signup route

---
+++ src/routers/router.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+router.post('/signup', (req, res, next) => {
+  let user = new User(req.body);
+  user.save()
+    .then((user) => {
+      req.token = user.generateToken();
+      req.user = user;
+      res.set('token', req.token);
+      res.cookie('auth', req.token);
+      res.send(req.token);
+    })
+    .catch(next);
+});
