f6a9db6e943910ad764c8a3487a93b846639a9a2
Jon Struve <ocdammo3@gmail.com>
2019-10-15 11:40:19 -0500

    Updated route names and paths

--- src/routers/category-routes.js
+++ src/routers/category-routes.js
@@ -4,50 +4,43 @@ const router = express.Router();
 const Categories = require('../models/categories/categories');
 const categories = new Categories();
 
-router.get('/', getCategories);
-router.post('/', postCategories);
-router.get('/:id', getCategory);
-router.put('/:id', putCategories);
-router.delete('/:id', deleteCategories);
+router.get('/', getHome);
+router.get('/categories/:name', getCategory);
+router.post('/categories/:name', postCategory);
+router.put('/categories/:name', putCategory);
 
 // ROUTE HANDLER FUNCTIONS
 
-function getCategories(request,response,next) {
+function getHome(request,response,next) {
   categories.get()
-    .then( data => {
+    .then(data => {
       const output = {
         count: data.length,
         results: data,
       };
       response.status(200).json(output);
     })
-    .catch( next );
+    .catch(next);
 }
 
 function getCategory(request,response,next) {
   categories.get(request.params.id)
-    .then( result => response.status(200).json(result) )
-    .catch( next );
+    .then(result => response.status(200).json(result))
+    .catch(next);
 }
 
-function postCategories(request,response,next) {
+function postCategory(request,response,next) {
   categories.create(request.body)
-    .then( result => {
+    .then(result => {
       response.status(200).json(result);
     } )
     .catch( next );
 }
 
-function putCategories(request,response,next) {
+function putCategory(request,response,next) {
   categories.update(request.params.id, request.body)
-    .then( result => response.status(200).json(result) )
-    .catch( next );
-}
-
-function deleteCategories(request,response,next) {
-  categories.delete(request.params.id)
-    .then( result => response.status(200).json(result) )
-    .catch( next );
+    .then(result => response.status(200).json(result))
+    .catch(next);
 }
 
 module.exports = router;
--- src/routers/product-routes.js
+++ src/routers/product-routes.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const Products = require('../models/products/products');
 const products = new Products();
 
-router.get('/', getProducts);
-router.post('/', postProducts);
-router.get('/:id', getProduct);
-router.put('/:id', putProducts);
-router.delete('/:id', deleteProducts);
+router.get('/products', getAllProducts);
+router.get('/products/:id', getProduct);
+router.post('/products', postProduct);
+router.put('/products/:id', putProduct);
+router.delete('/products/:id', deleteProducts);
 
-function getProducts(request,response,next) {
+function getAllProducts(request,response,next) {
   products.get()
     .then( data => {
       const output = {
@@ -28,13 +28,13 @@ function getProduct(request,response,next) {
     .catch( next );
 }
 
-function postProducts(request,response,next) {
+function postProduct(request,response,next) {
   products.create(request.body)
     .then( result => response.status(200).json(result) )
     .catch( next );
 }
 
-function putProducts(request,response,next) {
+function putProduct(request,response,next) {
   products.update(request.params.id, request.body)
     .then( result => response.status(200).json(result) )
     .catch( next );
